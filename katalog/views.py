614b4192f3e162c95fc88d02efcfa0701a10812f
Rayhan Putra Randi <rayhanrandi10@gmail.com>
2022-09-11 13:31:54 +0700

    Deploy w/o tests

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_barang_catalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_catalog,
+        'nama': 'Rayhan Putra Randi',
+        'id': '2106705644'
+    }
+    return render(request, "katalog.html", context)
